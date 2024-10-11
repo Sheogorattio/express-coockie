d6efc272924c5667cf07994320d4e405e414ff93
Sheogorattio <91422100+Sheogorattio@users.noreply.github.com>
2024-10-11 08:17:45 +0300

    .

--- src/middlewaares/user-middleware.js
+++ src/middlewaares/user-middleware.js
@@ -1,3 +1,7 @@
+import {users} from '../data/user.js'
+import bcrypt from "bcrypt"
+import validator from 'validator';
+
 export const checkUser = (req, res, next)=> {
     // if(req.cookies && req.cookies.user){
     //     res.locals.user = req.cookies.user;
@@ -6,4 +10,59 @@ export const checkUser = (req, res, next)=> {
         res.locals.user = req.session.user.login;
     }
     next ();
+}
+
+export const loginUser = (req,res, next) => {
+    const { login, password } = req.body;
+    
+    const user = users.find(u => u.login === login);
+    if (!user) {
+        return res.status(400).send('User does not exist');
+    }
+    
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+        return res.status(400).send('Invalid password');
+    }
+    
+    next();
+}
+
+export const createUser = (req, res, next)=>{
+    const reExp = new RegExp('^(?=.*\\w)(?=.*\\W)(?=.*\\d).{8,}');
+    if(req.body && 
+        req.body.login &&
+         req.body.email &&
+          req.body.password &&
+           req.body.confirm_password &&
+            req.body.confirm_password === req.body.password &&
+             reExp.test(req.body.password) &&
+                 validator.isEmail(req.body.email)){
+        const {login, email, password} = req.body;
+        const user = users.find(n => n.login === login || n.email === email);
+        if(!user){
+            const hash = hashPassword(password);
+            users.push({
+                id: users.length+1,
+                login: login,
+                email: email,
+                password: hash
+
+            });
+            next();
+            return;
+        }
+        res.status(400).redirect('/');
+    }
+}
+
+ function hashPassword(password) {
+    try {
+        const saltRounds = bcrypt.genSaltSync(10);
+        const hashedPassword =  bcrypt.hashSync(password, saltRounds);
+        return hashedPassword;
+    } catch (error) {
+        console.error('Error hashing password:', error);
+        throw error;
+    }
 }
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,54 +1,41 @@
 import {Router} from 'express'
-import session from 'express-session';
-import validator from 'validator';
-import bcrypt from "bcrypt"
+import {createUser, loginUser} from "../middlewaares/user-middleware.js"
+import {users} from "../data/user.js"
 
 const userRoutes = Router();
 
-userRoutes.get("/signup", (req,res)=> {res.render("form_register")});
-userRoutes.get("/signin", (req,res)=> {res.render("form_auth")});
+userRoutes.route('/')
+.get((req,res)=>{
+    res.json(users);
+})
+
+userRoutes.route("/signin")
+.get((req,res)=> {res.render("form_auth")})
+.post(loginUser, (req, res) => {
+    console.log(req.body.login);
+    req.session.user = {
+        login: req.body.login,
+        password: req.body.password
+    };
+    res.redirect("/");
+});
+
 userRoutes.get("/logout", (req,res)=> {
     console.log(req.session.user)
-    req.session.destroy(); 
+    if(req.session) req.session.destroy(); 
    // console.log(req.session.user)
     res.redirect("/")
 });
 
 userRoutes.route("/signup")
 .get((req,res)=> {res.render("form_register")})
-.post(async (req,res) => {
-    console.log(req.body);
-    // res.cookie("user", req.body.login, {
-    //     httpOnly: true,
-    //     maxAge: 2000000,
-    // });
-    const body = req.body;
-    const reExp = new RegExp('^(?=.*\\w)(?=.*\\W)(?=.*\\d).{8,}');
-    if(validator.isEmail(body.email) && body.password == body.confirm_password && reExp.test(body.password)){
-        req.session.user = {
-            login: body.login,
-            email: body.email,
-            password: await hashPassword(body.password),
-        }
-        // console.log(req.session.cookie)
-        // console.log(req.session.user)
-        res.redirect("/");
-    }
-    else{
-        console.log("Invalid password");
-        res.redirect("signup");
-    }
+.post(createUser, (req,res) => {
+    req.session.user = {
+        login: req.body.login,
+        password: req.body.password
+    };
+    res.redirect("/");
 });
 
-async function hashPassword(password) {
-    try {
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        return hashedPassword;
-    } catch (error) {
-        console.error('Error hashing password:', error);
-        throw error;
-    }
-}
 
 export default userRoutes;
